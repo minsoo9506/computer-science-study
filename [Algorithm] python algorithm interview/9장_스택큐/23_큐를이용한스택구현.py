9b94da0c419940758b526d596819bfa1e65d05af
minsoo9506 <ghktjd15gh@gmail.com>
2022-06-14 22:39:13 +0900

    algorithm study

---
+++ [Algorithm] python algorithm interview/9장_스택큐/23_큐를이용한스택구현.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/implement-stack-using-queues
+
+import collections
+
+
+class MyStack:
+    def __init__(self):
+        self.q = collections.deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        return self.q.pop()
+
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
