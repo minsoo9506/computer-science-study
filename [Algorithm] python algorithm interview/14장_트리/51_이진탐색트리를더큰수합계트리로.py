7b42a8691b39a8e885c831707b92d89f24967593
minsoo9506 <ghktjd15gh@gmail.com>
2022-07-13 22:56:16 +0900

    algorithm study (dfs, 중위순회)

---
+++ [Algorithm] python algorithm interview/14장_트리/51_이진탐색트리를더큰수합계트리로.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    val = 0
+
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        # 중위순회
+        if root:
+            self.bstToGst(root.right)
+            self.val += root.val
+            root.val = self.val
+            self.bstToGst(root.left)
+
+        return root
