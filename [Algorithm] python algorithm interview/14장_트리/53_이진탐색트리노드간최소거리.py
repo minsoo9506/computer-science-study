821c3bee4f3942e000426bc17e89b369dc967090
minsoo9506 <ghktjd15gh@gmail.com>
2022-08-01 21:52:05 +0900

    algorithm study (이진탐색트리, 중위순회)

---
+++ [Algorithm] python algorithm interview/14장_트리/53_이진탐색트리노드간최소거리.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/minimum-distance-between-bst-nodes/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        prev = -int(1e9)
+        result = int(1e9)
+
+        stack = []
+        node = root
+
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+
+            result = min(result, node.val - prev)
+            prev = node.val
+
+            node = node.right
+
+        return result
