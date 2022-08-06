24b200ecd9251bd18089759cb1007fb28d425dd3
minsoo9506 <ghktjd15gh@gmail.com>
2022-08-06 16:41:51 +0900

    algorithm study: trie 구현

---
+++ [Algorithm] python algorithm interview/16장_트라이/56_트라이구현.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/implement-trie-prefix-tree/
+
+
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.children[char]  # defaultdict 때문에 자동으로 생성
+        node.word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
